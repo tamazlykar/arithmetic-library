a1e2d97242ba7aa6276a93cdc6a428dd32e66800
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2016-11-09 19:13:04 +0700

    some new init

--- arithmetic-library.js
+++ arithmetic-library.js
@@ -1,10 +1,21 @@
+"use strict";
+
 var asmd = (function() {
   function asmd() {
 
   }
 
-  function add() {
-    console.log("this is add");
+  function add(a, b) {
+
+    let maxLength = Math.max(a.length, b.length);
+
+    let result = []
+
+    let memory
+    for (let i = maxLength-1, let j = ; i >= 0; i--) {
+      let sum = a
+      result.push()
+    }
   }
 
   return {
@@ -13,3 +24,5 @@ var asmd = (function() {
     }
   }
 }());
+
+//TODO JsDOC
---
+++ asd.js
@@ -0,0 +1,12 @@
+var asmd = (function() {
+  function asmd() {
+
+  }
+
+
+  return {
+    hw: function() {
+      console.log("Hello world");
+    }
+  }
+}());
