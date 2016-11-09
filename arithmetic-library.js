723a1ecfe539f1f7873fa46784e62b0394fec251
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2016-11-09 17:25:02 +0700

    init

---
+++ arithmetic-library.js
@@ -0,0 +1,16 @@
+var asmd = (function() {
+    function asmd() {
+
+    }
+
+    function add() {
+        console.log("this is add");
+    }
+
+    return {
+        hw: function() {
+            console.log("Hello world");
+        }
+    }
+
+}());
