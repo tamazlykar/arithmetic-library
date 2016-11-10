38d8b2064a62d03a61046dd1d7eb038eb0938e1d
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2016-11-10 15:25:11 +0700

    create add function and tests

--- arithmetic-library.js
+++ arithmetic-library.js
@@ -1,30 +1,61 @@
 'use strict';
 
-let asmd = (function() {
-  function asmd() {
+//TODO: check if all string is numeric
 
-  }
-
-  function add(a, b) {
+(function(exports) {
+  exports.add = function(a, b) {
     if (a.length < b.length) {
-      [a, b] = [b, a];
+      // node don't support this constructon [a,b]=[b,a]
+      b = [a, a = b][0];
     }
 
+    let arrayResult = [];
 
-    let result = [];
-
-    let memory;
-    for (let ai = a.length, bi = b.length; bi >= 0; ai--, bi--) {
-      let sum = a;
-      result.push();
+    let memory = '0';
+    let ai;
+    let bi;
+    for (ai = a.length-1, bi = b.length-1; bi >= 0; ai--, bi--) {
+      let sum = String(+a[ai] + +b[bi] + +memory);
+      if (sum.length == 2) {
+        memory = sum[0];
+        arrayResult.push(sum[1]);
+      } else {
+        memory = '0';
+        arrayResult.push(sum[0]);
+      }
     }
-  }
 
-  return {
-    add: function() {
-      return 1;
-    },
+    while (ai >= 0) {
+      let sum = String(+a[ai] + +memory);
+      if (sum.length == 2) {
+        memory = sum[0];
+        arrayResult.push(sum[1]);
+      } else {
+        memory = '0';
+        arrayResult.push(sum[0]);
+      }
+
+      ai--;
+    }
+
+    if (memory != '0') {
+      arrayResult.push(memory);
+    }
+
+    let result = '';
+    for (let i = arrayResult.length-1; i >= 0; i--) {
+      result += arrayResult[i];
+    }
+
+    return result;
   };
-}());
 
-// TODO JsDOC
+  exports.subtract = function() {
+
+  };
+
+  exports.multiply = function() {
+
+  };
+}(typeof exports === 'undifined' ? this['asmd']={} : exports));
+
--- test/test.js
+++ test/test.js
@@ -1,15 +1,58 @@
-var assert = require('chai').assert;
-var expect = require('chai').expect;
-var asmd = require('../arithmetic-library.js');
+'use strict';
+
+let assert = require('chai').assert;
+let expect = require('chai').expect;
+let asmd = require('../arithmetic-library.js');
 
 describe('Arithmetic Library (asmd)', function() {
   it('Should have method add', function() {
     expect(asmd).to.respondTo('add');
   });
+  it('Should have method subtract', function() {
+    expect(asmd).to.respondTo('subtract');
+  });
+  it('Should have method multiply', function() {
+    expect(asmd).to.respondTo('multiply');
+  });
 
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(asmd.add(), 1);
+  describe('Function add', function() {
+    describe('Not long numbers', function() {
+      it('add 0 to 5 equal 20', function() {
+        assert.equal(asmd.add('0', '5'), '5');
+      });
+
+      it('add 0 to 0 equal 0', function() {
+        assert.equal(asmd.add('0', '0'), '0');
+      });
+
+      it('add 5 to 15 equal 20', function() {
+        assert.equal(asmd.add('5', '15'), '20');
+      });
+
+      it('add 15 to 5 equal 20', function() {
+        assert.equal(asmd.add('15', '5'), '20');
+      });
+
+      it('add 999999 to 1 equal 1000000', function() {
+        assert.equal(asmd.add('999999', '1'), '1000000');
+      });
+
+      it('add 123456789 to 9876564321 equal 20', function() {
+        assert.equal(asmd.add('123456789', '987654321'), '1111111110');
+      });
+    });
+    describe('Long numbers', function() {
+      it('add 1 to 999999999999999999999999999999 equal 1000000000000000000000000000000', function() {
+        assert.equal(asmd.add('1', '999999999999999999999999999999'), '1000000000000000000000000000000');
+      });
+
+      it('add 4684684681168468468168151846846816168 to 468684681681681684844168168348 equal 4684685149853150149849836691014984516', function() {
+        assert.equal(asmd.add('4684684681168468468168151846846816168', '468684681681681684844168168348'), '4684685149853150149849836691014984516');
+      });
+
+      it('add 959269 to 936843684684136846143841618368413861368168866814 equal 4684685149853150149849836691014984516', function() {
+        assert.equal(asmd.add('959269', '936843684684136846143841618368413861368168866814'), '936843684684136846143841618368413861368169826083');
+      });
     });
   });
 });
